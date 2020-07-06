df7fd658c3dc565f0bbc8a385ad8f33a3bf1f7b2
Juliancio Carvalho <juliancio_alves@hotmail.com>
2020-07-06 08:58:58 -0300

    relation

--- src/modules/orders/infra/http/controller/OrdersController.ts
+++ src/modules/orders/infra/http/controller/OrdersController.ts
@@ -7,10 +7,25 @@ import FindOrderService from '@modules/orders/services/FindOrderService';
 
 export default class OrdersController {
   public async show(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { customer_id } = request.body;
+
+    const getOrders = container.resolve(FindOrderService);
+
+    const orders = await getOrders.execute(customer_id);
+
+    return response.json(orders);
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    // TODO
+    const { customer_id, products } = request.body;
+
+    const createOrder = container.resolve(CreateOrderService);
+
+    const order = await createOrder.execute({
+      customer_id,
+      products
+    });
+
+    return response.json(order);
   }
 }
